e32414b54607e60de71021e6d0bbc1c9bf9c8dc4
@Elephant9748 <adytamasurya@protonmail.com>
2025-10-01 15:16:06 +0700

    from totp url failed msg

--- src/utils/totp.rs
+++ src/utils/totp.rs
@@ -68,7 +68,8 @@ pub fn totp_create(params: &str, timeout: i32) {
 
     // get otp url from plaintext
     let plainvec: Vec<&str> = plaintext.split("\n").collect();
-    let totprs = TOTP::from_url(plainvec[2]).expect("Url is empty not allow in TOTP::from_url()");
+    let totprs = TOTP::from_url(plainvec[2])
+        .expect(format!("{}", "Totp::try_from error ->".bright_red()).as_str());
 
     //timestamp
     let timestamp: DateTime<Utc> = Utc::now();
