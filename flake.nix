7c5b77ab985b8fe1b59255c1ebdea6d76e81d4d3
@Elephant9748 <adytamasurya@proton.me>
2026-07-25 02:54:33 +0700

    nix: oops

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        pkgs = nixpkgs.legacyPackages.${system};
         formattedDate = builtins.readFile (
                 pkgs.runCommand "date" {
                         buildInputs = [ pkgs.coreutils ];
